37f895c8cc837f22a787d14d5e5f8a1752f0ac9b
LogotipX <Starikov1000@gmail.com>
2022-08-23 20:28:08 +0500

    fix: popup position

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 function onBlur(elementId) {
   document.getElementById(elementId).style.display = "none";
+  document.getElementsByTagName("body")[0].style.overflow = "visible";
 }
 
 function showPopup(elementId) {
+  document.getElementById(elementId).style.top = `${scrollY}px`;
   document.getElementById(elementId).style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
 }

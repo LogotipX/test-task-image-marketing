e4dd356b9efe012b186e84f35ce3f32d45ef991c
LogotipX <Starikov1000@gmail.com>
2022-08-23 19:54:21 +0500

    feat: add popup and script

---
+++ popup.js
@@ -0,0 +1,7 @@
+function onBlur(elementId) {
+  document.getElementById(elementId).style.display = "none";
+}
+
+function showPopup(elementId) {
+  document.getElementById(elementId).style.display = "block";
+}
